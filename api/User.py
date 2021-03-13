3d99a9751b85e72aa7aa7595e1538ebf918897a3
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-03-13 16:02:30 -0300

    update user and user by id

--- api/User.py
+++ api/User.py
@@ -1,15 +1,12 @@
 from flask_restful import Resource
 import logging as logger
 
-class User():
-    def get_user(self):
-        logger.debug("inside get method")
-        pass
-    
-    def create_user(self):
-        logger.debug("inside create method")
-        pass
 
-    def get_all_users(self):
-        logger.debug("inside get ALL method")
-        pass
+class User(Resource):
+    def get(self):
+        logger.debug("inside get method")
+        return { "message": "get" }, 200
+
+    def post(self):
+        logger.debug("inside create method")
+        return { "message": "post" }, 201
---
+++ api/user_by_id.py
@@ -0,0 +1,8 @@
+from flask_restful import Resource
+import logging as logger
+
+
+class UserById(Resource):
+    def get(self, user_id):
+        logger.debug("inside get method")
+        return { "message": user_id }, 200
