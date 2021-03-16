a87f4ffea8869a569c4dd439c5aef837e6c4f4bd
Diego Prestes (DPS) <diegoprestes.git@hotmail.com>
2021-03-15 23:58:32 -0300

    feat: create user searching by id

--- api/database.py
+++ api/database.py
@@ -1,4 +1,5 @@
 import psycopg2
+import uuid
 
 
 def create_connection():
@@ -21,8 +22,32 @@ def get_all_users():
     for row in rows:
         user = (f'id: {row[0]}, name: {row[1]}')
         users.append(user)
-    
+
     cursor.close()
     connection.close()
 
     return users
+
+
+def get_user_by_id(user_id):
+    connection = create_connection()
+    cursor = connection.cursor()
+    cursor.execute('''SELECT * FROM users WHERE id = {0}'''.format(user_id))
+    user = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return user
+
+
+def create_user(name, email, password):
+    connection = create_connection()
+    cursor = connection.cursor()
+    user_id = uuid.uuid1()
+    cursor.execute(
+        '''INSERT INTO users(id, name, password)
+        VALUES({0}, {1}, {2})'''.format(user_id, name, email, password))
+
+    cursor.close()
+    connection.close()
--- api/user_by_id.py
+++ api/user_by_id.py
@@ -1,8 +1,8 @@
 from flask_restful import Resource
 import logging as logger
-
+from .database import get_user_by_id
 
 class UserById(Resource):
     def get(self, user_id):
-        logger.debug("inside get method")
-        return {"message": user_id}, 200
+        selected_user = get_user_by_id(user_id)
+        return selected_user, 200
